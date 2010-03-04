3075bab5bb3dd0298e3d2012fe7ac1d60c0a8d09
Emmanuel Goossaert <emmanuel.goossaert@gmail.com>
2010-03-04 20:19:53 +0000

    Add: setup.py installer.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='doge',
+      version='1.0',
+      description='Documentation generator',
+      author='Emmanuel Goossaert',
+      author_email='',
+      url='',
+      packages=['src'],
+      )
+
